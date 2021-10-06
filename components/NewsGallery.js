5a9dd041c21a2b8d3842924601e66998b4bc321e
Mauricio Mossi <mauricio.b.mossi@gmail.com>
2021-10-06 12:55:05 -0400

    adding keys to maps

--- components/NewsGallery.js
+++ components/NewsGallery.js
@@ -24,11 +24,11 @@ export default function NewsGallery({ post }) {
       <div className="grid xl:grid-cols-4 lg:p-4 2xl:p-20 gap-8">
         <div className="col-span-3 flex ">
           <div className="grid grid-cols-3 gap-10 lg:gap-20 border-r border-l ">
-            {largeNewsBox.map((post) => (
-              <LargeNewsBox post={post} />
+            {largeNewsBox.map((post, key) => (
+              <LargeNewsBox key={key} post={post} />
             ))}
-            {mediumNewsBox.map((post) => (
-              <MediumNewsBox post={post} />
+            {mediumNewsBox.map((post, key) => (
+              <MediumNewsBox key={key} post={post} />
             ))}
           </div>
         </div>
@@ -36,8 +36,8 @@ export default function NewsGallery({ post }) {
         <div className="col-span-3 xl:order-first xl:col-span-1 h-full xl:flex">
           <div className="flex  justify-center items-center lg:justify-self-auto  lg:items-stretch  ">
             <div className="grid xl:grid-cols-1 gap-8 justify-center items-center max-w-lg ">
-              {smallNewsBox.map((post) => (
-                <SmallNewsBox post={post} />
+              {smallNewsBox.map((post, key) => (
+                <SmallNewsBox key={key} post={post} />
               ))}
             </div>
           </div>

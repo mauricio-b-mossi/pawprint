5eff69f82bc16b551bf6e7dbccd0d386e7ccb930
mauricio-b-mossi <mauricio.b.mossi@gmail.com>
2022-10-06 02:11:27 -0400

    cleaned meet the team page

--- components/Members.js
+++ components/Members.js
@@ -28,27 +28,45 @@ const Members = ({ member }) => {
       >
         <div className="flex flex-col justify-center items-center">
           <div className="w-40 h-40 md:w-52 md:h-52 relative">
+
+            {author?.bio ? 
             <Image
               className="rounded-full cursor-pointer "
               src={author?.image.asset.url ?? pawprint}
               alt=""
-              width="300"
-              height="300"
+              width="299"
+              height="299"
               priority={true}
-              
-              onClick={author?.bio ?? toggleOpen}
+              onClick={toggleOpen}
+            /> : 
+            <Image
+              className="rounded-full"
+              src={author?.image.asset.url ?? pawprint}
+              alt=""
+              width="299"
+              height="299"
+              priority={true}
             />
+          }
+
+            
           </div>
 
           {/* If there is no bio do not let tap */}
           <AnimatePresence>
             <div className="block p-2">{isOpen && <Content text={author?.bio} />}</div>
           </AnimatePresence>
+          {author?.name ?
           <Link href={"/members/" + author?.slug.current}>
             <a>
               <h1 className="font-black pt-3 hover:underline">{author?.name ?? "Vacant"}</h1>
             </a>
-          </Link>
+          </Link> :
+            <a>
+              <h1 className="font-black pt-3">{author?.name ?? "Vacant"}</h1>
+            </a>
+          }
+
           <h3 className="font-light">{position ?? ""}</h3>
         </div>
       </motion.div>
